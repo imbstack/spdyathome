8a8f626ee33de3da1ed828f660c79e02af320a7d
Brian Stack <bis12@case.edu>
2012-11-10 14:47:38 -0500

    Do status codes and such correctly

--- spdyathome/serverbase.py
+++ spdyathome/serverbase.py
@@ -3,6 +3,7 @@ Base class for common operations of servers.
 """
 import logging
 import util
+import json
 
 
 class BaseServer(object):
@@ -12,31 +13,34 @@ class BaseServer(object):
         util.register_all(self)
 
     def handler(self, method, uri, hdrs, res_start, req_pause):
-        res_body, res_done = res_start(200, 'OK', {}, req_pause)
 
-        self.paths.get(util.make_ident(method, uri), self.fourohfour)(res_body)
-        res_done(None)
+        self.paths.get(util.make_ident(method, uri), self.fourohfour)(res_start)
 
         # FIXME: The following are totally not the right thing to return
-        return res_body, res_done
+        return lambda x: x, lambda y: y
 
     def get_logger(self, name):
         log = logging.getLogger(name)
         log.setLevel(logging.INFO)
         return log
 
-    def dispatch(self):
-        return NotImplemented
-
     # HTTP verbs and paths below
 
     # FIXME: This sort of situation should return status code 404, not 200!
-    def fourohfour(self, res_body):
+    def fourohfour(self, res_start):
+        headers = []
+        res_body, res_done = res_start(404, 'Error Not Found', headers, None)
         res_body('THAT DOESN\'T EXIST!')
+        res_done(None)
 
     """
     Send the list of sites to access.
     """
     @util.register(method='GET', path='/hello')
-    def gethello(self, res_body):
-        res_body("HELLO THERE!")
+    def gethello(self, res_start):
+        headers = []
+        # FIXME: This should not have none for pause
+        res_body, res_done = res_start(200, 'OK', headers, None)
+        # TODO: Generate this list from a file of top 100, etc.
+        res_body(json.dumps([1,77,23,11]))
+        res_done(None)
