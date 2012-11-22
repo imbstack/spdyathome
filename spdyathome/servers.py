daecff1408c6946013041d186d696c76763a3809
Brian Stack <bis12@case.edu>
2012-11-21 19:21:19 -0500

    Update SPDY to use the Thor server

--- spdyathome/servers.py
+++ spdyathome/servers.py
@@ -1,14 +1,13 @@
 """
-Start the basic nbhttp servers for each case of connection.
+Start the basic thor servers for each case of connection.
 Use the standard request responders in BaseServer.
 """
 import yaml
 import logging
 import argparse
 from thor.loop import run
-from nbhttp.spdy_server import SpdyServer
-from nbhttp import push_tcp
 from thor import HttpServer
+from thor import SpdyServer
 from serverbase import BaseServer
 from multiprocessing import Process
 from thor.events import on
@@ -26,8 +25,7 @@ def get_args():
     return parser.parse_args()
 
 
-def http_main(cfile):
-    conf = yaml.load(file(cfile, 'r'))
+def http_main(conf):
     print 'Loading configuration into http base server!'
     base = BaseServer(conf)
     print 'creating HTTP server on port %d' % (conf['http_port'],)
@@ -40,44 +38,44 @@ def http_main(cfile):
 
         @on(x, 'request_body')
         def body(chunk):
-            print 'body: %s' % chunk
+            #print 'body: %s' % chunk
+            pass
 
         @on(x, 'request_done')
         def done(trailers):
-            print 'done: %s' % str(trailers)
+            #print 'done: %s' % str(trailers)
+            pass
 
     http_serve = HttpServer(host='', port=conf['http_port'])
     http_serve.on('exchange', http_handler)
-    run()
 
 
-def spdy_main(cfile):
-    conf = yaml.load(file(cfile, 'r'))
-    log = logging.getLogger('SPDY')
+def spdy_main(conf):
     print 'Loading configuration into spdy base server!'
     base = BaseServer(conf)
     print 'creating SPDY server on port %d' % (conf['spdy_port'],)
 
-    def spdy_handler(method, uri, hdrs, res_start, req_pause):
-        path = urilib.URI(uri).path
-        print 'SPDY: start %s' % (path,)
-        met = base.paths.get(util.make_ident(method, path), base.fourohfour)
-        met(util.resmap(res_start, uri))
+    def spdy_handler(x):
+        @on(x, 'request_start')
+        def go(*args):
+            print 'SPDY: start %s' % (str(args[1]),)
+            base.paths.get(util.make_ident(x.method, x.uri), base.fourohfour)(x)
 
+        @on(x, 'request_body')
         def body(chunk):
-            print 'body: %s' % chunk
+            #print 'body: %s' % chunk
+            pass
 
+        @on(x, 'request_done')
         def done(trailers):
-            print 'done: %s' % str(trailers)
+            #print 'done: %s' % str(trailers)
+            pass
 
-        return  body, done
-
-    SpdyServer('0.0.0.0', conf['spdy_port'], spdy_handler, log)
-    push_tcp.run()
+    spdy_serve = SpdyServer('', conf['spdy_port'])
+    spdy_serve.on('exchange', spdy_handler)
 
 
-def capture_main(cfile):
-    conf = yaml.load(file(cfile, 'r'))
+def capture_main(conf):
     print 'creating capture server on port %d' % (conf['capture_port'],)
 
     def capture_handler(x):
@@ -102,7 +100,6 @@ def capture_main(cfile):
 
     capture_serve = HttpServer(host='', port=conf['capture_port'])
     capture_serve.on('exchange', capture_handler)
-    run()
 
 if __name__ == '__main__':
     args = get_args()
@@ -113,15 +110,7 @@ if __name__ == '__main__':
     except OSError:
         pass
 
-    capture = Process(target=capture_main, args=(args.conf_file,))
-    capture.start()
-
-    spdy = Process(target=spdy_main, args=(args.conf_file,))
-    spdy.start()
-
-    http = Process(target=http_main, args=(args.conf_file,))
-    http.start()
-
-    spdy.join()
-    http.join()
-    capture.join()
+    capture_main(conf)
+    spdy_main(conf)
+    http_main(conf)
+    run()
