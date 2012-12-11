d80159a54567935caa8f166a66b75d21937e5e15
Brian Stack <bis12@case.edu>
2012-12-10 19:54:27 -0500

    Add first 3 plots

---
+++ analysis/plot3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+3. average over the course of a whole responder  otherwise same as plot1
+"""
+import sys
+import json
+
+if __name__ == '__main__':
+    f = open(sys.argv[1])
+    speed_diffs = []
+    for line in f:
+        sd = []
+        blob = json.loads(line)
+        for site,vals in blob.items():
+            if type(vals) != dict:
+                continue
+            spdy = float(vals['spdy']['time'])
+            http = float(vals['http']['time'])
+            sd.append(http - spdy)
+        speed_diffs.append(sum(sd)/len(sd))
+    for x in sorted(speed_diffs):
+        print x
---
+++ analysis/plot9.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+9. same as plot1 except average across site
+"""
+import sys
+import json
+
+if __name__ == '__main__':
+    f = open(sys.argv[1])
+    speed_diffs = {}
+    for line in f:
+        blob = json.loads(line)
+        for site,vals in blob.items():
+            if type(vals) != dict:
+                continue
+            spdy = float(vals['spdy']['time'])
+            http = float(vals['http']['time'])
+            if site in speed_diffs:
+                speed_diffs[site].append(http - spdy)
+            else:
+                speed_diffs[site] = [http - spdy]
+    avgs = [sum(x)/len(x) for x in speed_diffs.values()]
+    for x in sorted(avgs):
+        print x
